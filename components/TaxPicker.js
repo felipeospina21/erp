2b5af26eaab1bf3123727327115146aff78d05f0
felipe ospina calle <felipeospina21@hotmail.com>
2021-10-11 16:36:08 -0500

    add TaxPicker component

---
+++ components/TaxPicker.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { HStack, Box, Input } from "@chakra-ui/react";
+
+
+const TaxPicker = (props) => {
+  
+  const handleChange= (event) =>{
+    props.setTax(event.target.value);
+  }
+  return (
+    <HStack textAlign='right' fontSize='1.5rem' mt='1.5rem' w='100%'>
+      <Box w='93%'>IVA (%): </Box>
+      <Input textAlign='right' w='7%' size='xl' variant="unstyled" value={props.value} onChange={handleChange}/>
+    </HStack>
+  );
+};
+
+export default TaxPicker;
