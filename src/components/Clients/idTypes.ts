7fb7743192e644bb646a3e7e041449a550da4b44
Felipe Ospina <51976045+felipeospina21@users.noreply.github.com>
2022-06-28 17:51:14 -0500

    fix: payment select bug

---
+++ src/components/Clients/idTypes.ts
@@ -0,0 +1,6 @@
+export const idTypes = [
+  { _id: 'CC', name: 'CC' },
+  { _id: 'NIT', name: 'NIT' },
+  { _id: 'Pasaporte', name: 'Pasaporte' },
+  { _id: 'Otro', name: 'Otro' },
+];
---
+++ src/components/Clients/paymentTerms.ts
@@ -0,0 +1,6 @@
+export const paymentTerms = [
+  { _id: 'contado', name: 'contado' },
+  { _id: '15', name: '15' },
+  { _id: '30', name: '30' },
+  { _id: '60', name: '60' },
+];
