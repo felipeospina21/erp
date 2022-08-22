e1baf035ffa61addf8d134256e81e7ff006cae08
Felipe Ospina <51976045+felipeospina21@users.noreply.github.com>
2022-08-22 12:11:13 -0500

    update client mockData and add client msw handler

--- src/utils/test-utils/mockData/client.ts
+++ src/utils/test-utils/mockData/client.ts
@@ -14,3 +14,34 @@ export const clientMock: Client = {
   paymentTerm: '15',
   retailer: true,
 };
+
+export const clients: Client[] = [
+  {
+    _id: 'asdfkj',
+    addres1: 'calle 1',
+    addres2: 'calle 2',
+    city: 'medellin',
+    department: 'antioquia',
+    discount: 10,
+    email: 'email@email.com',
+    idNumber: '123456890',
+    idType: 'nit',
+    name: 'test client',
+    paymentTerm: '15',
+    retailer: true,
+  },
+  {
+    _id: 'zxcvaronpp',
+    addres1: 'calle 1',
+    addres2: 'calle 2',
+    city: 'medellin',
+    department: 'antioquia',
+    discount: 10,
+    email: 'email@email.com',
+    idNumber: '99999999',
+    idType: 'nit',
+    name: 'test client 2',
+    paymentTerm: '15',
+    retailer: true,
+  },
+];
