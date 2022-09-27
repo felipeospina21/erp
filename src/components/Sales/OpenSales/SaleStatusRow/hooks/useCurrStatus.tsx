133f9323fd4e36fcd5e0080813c47b5a73204196
Felipe Ospina <51976045+felipeospina21@users.noreply.github.com>
2022-09-27 16:52:01 -0500

    add useCurrStatus hook

---
+++ src/components/Sales/OpenSales/SaleStatusRow/hooks/useCurrStatus.tsx
@@ -0,0 +1,35 @@
+import { useGetSalesQuery, useUpdateSaleStatusMutation } from '@/redux/services';
+import { useState, useEffect, useCallback } from 'react';
+import type { SetStateAction, Dispatch } from 'react';
+
+export type UseCurrStatus = [
+  currStatus: string | undefined,
+  setCurrStatus: Dispatch<SetStateAction<string | undefined>>
+];
+
+export function useCurrStatus(
+  status: string | undefined,
+  saleId: string | undefined
+): UseCurrStatus {
+  const [currStatus, setCurrStatus] = useState(status);
+  const [updateSaleStatus] = useUpdateSaleStatusMutation();
+
+  const { cachedSale } = useGetSalesQuery(undefined, {
+    selectFromResult: ({ data }) => ({
+      cachedSale: data?.find((sale) => sale._id === saleId),
+    }),
+  });
+
+  const updateStatus = useCallback((args) => {
+    updateSaleStatus(args);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (currStatus && saleId && cachedSale?.status !== currStatus) {
+      updateStatus({ id: saleId, status: currStatus });
+    }
+  }, [currStatus, status, cachedSale?.status, saleId, updateStatus]);
+
+  return [currStatus, setCurrStatus];
+}
