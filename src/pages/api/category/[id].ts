94e26d9fa331cbf0105b84ec959c0e1a80016f63
Felipe Ospina <felipeospina21@hotmail.com>
2022-11-19 17:59:23 -0500

    migrate category controller

---
+++ src/pages/api/category/[id].ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { CategoryModel } from '@/server/models';
+import { dbConnect, controllerResponse } from '@/server/utils';
+import { findById } from '@/server/mongoose/helpers';
+
+export default async function categoryIdHandler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    method,
+    query: { id },
+  } = req;
+  await dbConnect();
+
+  switch (method) {
+    case 'GET':
+      const product = findById(CategoryModel, id as string);
+      controllerResponse(product, 200, 400, res);
+      break;
+  }
+}
---
+++ src/pages/api/category/index.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { Category } from '@/server/models';
+import { CategoryModel } from '@/server/models';
+import { dbConnect, controllerResponse } from '@/server/utils';
+import { findAll, createNewElement, deletetById } from '@/server/mongoose/helpers';
+
+export default async function categoryHandler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  await dbConnect();
+
+  switch (method) {
+    case 'GET':
+      const categories = findAll(CategoryModel);
+      controllerResponse(categories, 200, 400, res);
+      break;
+
+    case 'POST':
+      const newCategory = createNewElement(CategoryModel, req.body);
+      controllerResponse(newCategory, 201, 400, res);
+      break;
+
+    case 'DELETE':
+      const { _id: id } = req.body as Category;
+      const deletedCategory = deletetById(CategoryModel, id);
+      controllerResponse(deletedCategory, 200, 400, res);
+      break;
+  }
+}
