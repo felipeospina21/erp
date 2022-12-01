ff48fde8062623ca96b01dc124961022aafa424e
Felipe Ospina <felipeospina21@hotmail.com>
2022-12-01 15:26:18 -0500

    refactor login tests

--- __test__/login.test.tsx
+++ __test__/login.test.tsx
@@ -13,7 +13,7 @@ beforeEach(() => {
   mockRouter.setCurrentUrl('/login');
 });
 
-test.skip('On successful login it should redirect to Home page', async () => {
+test('On successful login it should redirect to Home page', async () => {
   const user = userEvent.setup();
   render(<Login />);
   await user.type(screen.getByLabelText(/email/i), 'test@email.com');
@@ -25,35 +25,35 @@ test.skip('On successful login it should redirect to Home page', async () => {
 });
 
 describe('On login error it should not redirect &', ()=>{
-  it.skip('should render wrong password error toast', async () => {
+  it('should render wrong password error toast', async () => {
     const user = userEvent.setup();
     render(<Login />);
     await user.type(screen.getByLabelText(/email/i), 'unauthorized@email.com');
     await user.type(screen.getByLabelText(/password/i), '1234567890');
     await user.click(screen.getByRole('button', { name: /login/i }));
-    const alert = await screen.findByRole('alert');
+    const alert = await screen.findByRole('status');
     expect(within(alert).getByText(/wrong password/i)).toBeInTheDocument();
     expect(Router.pathname).toBe('/login')
   });
 
-  it.skip('should render user not found error toast', async () => {
+  it('should render user not found error toast', async () => {
     const user = userEvent.setup();
     render(<Login />);
     await user.type(screen.getByLabelText(/email/i), 'notfound@email.com');
     await user.type(screen.getByLabelText(/password/i), '1234567890');
     await user.click(screen.getByRole('button', { name: /login/i }));
-    const alert = await screen.findByRole('alert');
+    const alert = await screen.findByRole('status');
     expect(within(alert).getByText(/user not found/i)).toBeInTheDocument();
     expect(Router.pathname).toBe('/login')
   });
 
-  it.skip('should render network error toast', async () => {
+  it('should render network error toast', async () => {
     const user = userEvent.setup();
     render(<Login />);
     await user.type(screen.getByLabelText(/email/i), 'nonetwork@email.com');
     await user.type(screen.getByLabelText(/password/i), '1234567890');
     await user.click(screen.getByRole('button', { name: /login/i }));
-    const alert = await screen.findByRole('alert');
+    const alert = await screen.findByRole('status');
     expect(within(alert).getByText(/network error/i)).toBeInTheDocument();
     expect(Router.pathname).toBe('/login')
   });
